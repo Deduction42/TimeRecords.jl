ce294a34106125db81b2d3b04b7332188b376015
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-09-27 11:21:25 -0600

    Modified operators to avoid invalidations

--- src/math.jl
+++ src/math.jl
@@ -1,3 +1,13 @@
+const Arithmetics = Union{Number, AbstractArray, Missing}
+const OPERATORS = (:+, :-, :*, :/, :^, :div, :mod)
+const MATHFUNCS = (
+        :-, :inv, :sin, :cos, :tan, :sinh, :cosh, :tanh, :asin, :acos, :atan,
+        :asinh, :acosh, :atanh, :sec, :csc, :cot, :asec, :acsc, :acot, :sech, :csch,
+        :coth, :asech, :acsch, :acoth, :sinc, :cosc, :cosd, :cotd, :cscd, :secd,
+        :sinpi, :cospi, :sind, :tand, :acosd, :atand, :acotd, :acscd, :asecd, :asind,
+        :log, :log2, :log10, :log1p, :exp, :exp2, :exp10, :expm1, :frexp, :exponent, :factorial,
+        :float, :abs, :real, :imag, :conj, :adjoint, :unsigned, :nextfloat, :prevfloat, :transpose, :significand
+    )
 
 function common_timestamp(arg1::TimeRecord, arg2::TimeRecord, args::TimeRecord...)
     t1 = timestamp(arg1)
@@ -23,21 +33,14 @@ end
 apply2values(f, arg::TimeRecord) = TimeRecord(timestamp(arg), f(value(arg)))
 
 #Two-argument operations
-for op in (:+, :-, :*, :/, :^, :div, :mod)
-    @eval Base.$op(x1::TimeRecord, x2::TimeRecord) = apply2values($op, x1, x2)
-    @eval Base.$op(x1::TimeRecord, x2) = TimeRecord(timestamp(x1), $op(value(x1),x2))
-    @eval Base.$op(x1, x2::TimeRecord) = TimeRecord(timestamp(x2), $op(value(x2),x1))
+for op in OPERATORS
+    @eval Base.$op(x1::TimeRecord, x2::TimeRecord)  = apply2values($op, x1, x2)
+    @eval Base.$op(x1::TimeRecord, x2::Arithmetics) = TimeRecord(timestamp(x1), $op(value(x1),x2))
+    @eval Base.$op(x1::Arithmetics, x2::TimeRecord) = TimeRecord(timestamp(x2), $op(value(x2),x1))
 end
 
 #Single-argument operations
-for f in (
-        :-, :inv, :sin, :cos, :tan, :sinh, :cosh, :tanh, :asin, :acos, :atan,
-        :asinh, :acosh, :atanh, :sec, :csc, :cot, :asec, :acsc, :acot, :sech, :csch,
-        :coth, :asech, :acsch, :acoth, :sinc, :cosc, :cosd, :cotd, :cscd, :secd,
-        :sinpi, :cospi, :sind, :tand, :acosd, :atand, :acotd, :acscd, :asecd, :asind,
-        :log, :log2, :log10, :log1p, :exp, :exp2, :exp10, :expm1, :frexp, :exponent, :factorial,
-        :float, :abs, :real, :imag, :conj, :adjoint, :unsigned, :nextfloat, :prevfloat, :transpose, :significand
-    )
+for f in MATHFUNCS
     @eval Base.$f(x::TimeRecord) = apply2values($f, x)
 end
 
